f5e9b617874e4f828041ba8b55390220792d8931
Boyan Binev <BGBinev18@codingburgas.bg>
2023-01-03 15:41:56 +0200

    Initial commit

---
+++ src/main/java/com/example/Boyan/BoyanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Boyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoyanApplication.class, args);
+	}
+
+}
